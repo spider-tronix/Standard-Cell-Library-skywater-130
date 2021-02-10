01bfdf3eac8061bcb23c7244e8200bdf18456891
akilm <akilmv555@gmail.com>
2021-02-10 14:33:24 +0530

    Characterization Excel Write Complete

--- Python Files/Characterization.py
+++ Python Files/Characterization.py
@@ -52,7 +52,7 @@ def NgSpice_Run(input_nodes,input_slew,output_capacitance):
                     
     print("NGSpice Simulation Runs Successfully completed")
 
-def Characterization_run(input_nodes,input_slew,output_capacitance,pulse_node):
+def Characterization_run(input_nodes,input_slew,output_capacitance,pulse_node,slew_lower_threshold,slew_upper_threshold,Vdd):
     filename = pulse_node+"_"+input_slew + "_" + output_capacitance +  ".data"
     #filename = "A_0.6n_0.1.data"
     file = open(filename,"r")
@@ -82,9 +82,9 @@ def Characterization_run(input_nodes,input_slew,output_capacitance,pulse_node):
             print("Vin Read Complete")
         values.clear()
         
-    Vdd = 1.8
-    V_out_upper_threshold = 0.8*Vdd
-    V_out_lower_threshold = 0.2*Vdd
+    
+    V_out_upper_threshold = (slew_uppper_threshold/100)*Vdd
+    V_out_lower_threshold = (slew_lower_threshold/100)*Vdd
     V_out_mid_threshold = 0.5*Vdd
 
     V_out_upper_line = list()
@@ -101,7 +101,7 @@ def Characterization_run(input_nodes,input_slew,output_capacitance,pulse_node):
                 V_out_upper_line_slope.append(-1)
             else:
                 V_out_upper_line_slope.append(1)
-            print(str(V_out_upper_threshold) + " " + str(Vout[i]))
+            #print(str(V_out_upper_threshold) + " " + str(Vout[i]))
         if(abs(Vout[i] - V_out_lower_threshold)<0.1):
             V_out_lower_line.append(i)
             if(Vout[i]-Vout[i-1]<0):
@@ -294,144 +294,186 @@ def Delete_Files(input_nodes,input_slew,output_capacitance):
                 print("Deleted file " + input_nodes[i]+"_"+input_slew[j] + "_" + output_capacitance[k] +  ".data")
 
         
+def Excel_writer(name,workbook,input_nodes,output_capacitance,input_slew,t):
+    bold = workbook.add_format({'bold': 1})
+    for k in range(len(input_nodes)):
+        worksheet = workbook.add_worksheet(name +" IN " + input_nodes[k])
+        worksheet.set_column('A:A', 35) 
+        start = 'B'
+        end = chr(ord(start)+len(input_slew))
+        worksheet.set_column(start+":"+end, 30)
+        
+        column = 'B'
+        row = 1
+        ## Write Row Header
+        for i in range(len(input_slew)):
+            index = column + str(row)
+            worksheet.write(index, str(input_slew[i]) + " ns",bold)
+            column = chr(ord(column)+1)
 
+        column = 'A'
+        row = 2
+        
+        ## Write Column Header
+        for j in range(len(output_capacitance)):
+            index = column + str(row)
+            worksheet.write(index, str(output_capacitance[j]) + " pF",bold)
+            row = row+1
+
+        column = 'B'
+        row = 2
+        m=0
+
+        table_size = len(input_slew)*len(output_capacitance)
+        for i in range(len(input_slew)):
+            for j in range(len(output_capacitance)):
+                index = chr(ord(column)+i) + str(row+j)
+                worksheet.write(index, str(t[m+k*table_size]) + " ns")
+                m=m+1
 
 ##---- Main Function----##
                 
-def main():
+##def main():
 
-    ##----List The spice files in the directory and get the choice----##
-    Netlists_Total = os.listdir("../Characterization/Netlists")
-    i = 1 
-    while(i<len(Netlists_Total)):
-        if(Netlists_Total[i].endswith(".cir")) == False:
-            Netlists_Total.pop(i)
-        else:
-            i=i+1
-    for i in range(len(Netlists_Total)):
-        print( str(i) + " : " + Netlists_Total[i])
+##----List The spice files in the directory and get the choice----##
+Netlists_Total = os.listdir("../Characterization/Netlists")
+i = 1 
+while(i<len(Netlists_Total)):
+    if(Netlists_Total[i].endswith(".cir")) == False:
+        Netlists_Total.pop(i)
+    else:
+        i=i+1
+for i in range(len(Netlists_Total)):
+    print( str(i) + " : " + Netlists_Total[i])
 
-    print("\n")
-    choice = input("Enter the index of the circuit to be characterised \n")
-    choice_file = Netlists_Total[int(choice)]
+print("\n")
+choice = input("Enter the index of the circuit to be characterised \n")
+choice_file = Netlists_Total[int(choice)]
+
+##---- Read the csv file for input datas----##
+input_read_file = choice_file.split(".")[0]
+data = list()
+with open("../Characterization/Netlists/"+input_read_file + '.csv', newline='') as csvfile:
+    input_data = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in input_data:
+        data.append(row)
+        
+input_nodes = list()
+i=1
+temp = str(data[0][0]).split(",")
+while(i<len(temp)):
+    if temp[i].isalnum() == True :
+        input_nodes.append(temp[i])
+    i=i+1
+
+output_nodes = list()
+i=1
+temp = str(data[1][0]).split(",")
+while(i<len(temp)):
+    if temp[i].isalnum() == True :
+        output_nodes.append(temp[i])
+    i=i+1
+
+Vdd = float(data[2][0].split(",")[1])
+T = float(data[3][0].split(",")[1])
+
+input_slew = list()
+i=1
+temp = str(data[4][0]).split(",")
+while(i<len(temp)):
+    if len(temp) > 0 :
+        m = str(float(temp[i][:-1])*100/60)+'n'
+        print(m)
+        input_slew.append(m)
+    i=i+1
+
+output_capacitance = list()
+i=1
+temp = str(data[5][0]).split(",")
+while(i<len(temp)):
+    if len(temp[i]) > 0 :
+        output_capacitance.append(temp[i])
+    i=i+1
+
+slew_lower_threshold = float(data[6][0].split(",")[1])
+slew_upper_threshold = float(data[7][0].split(",")[1])
+
+##-----Processing Netlist-----##
     
-    ##---- Read the csv file for input datas----##
-    input_read_file = choice_file.split(".")[0]
-    data = list()
-    with open("../Characterization/Netlists/"+input_read_file + '.csv', newline='') as csvfile:
-        input_data = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in input_data:
-            data.append(row)
+f = open("../Characterization/Netlists/"+ choice_file, "r")
+circuit = f.read()
+circuit = circuit.split("\n")
+circuit.remove(".end")
+circuit.append("\nV1 Vdd 0 " + str(Vdd))
+source_circuit = circuit
+temp_circuit = list()
+cir = ''
+print("Source Circuit Read")
+print("Building Netlists")
+
+##----- Create Netlists for all different combinations-----##
+for i in range(len(input_nodes)):
+    for j in range(len(input_slew)):
+        for k in range(len(output_capacitance)):
+            cir = Netlist_Build(output_nodes,input_nodes,input_nodes[i],input_slew[j],output_capacitance[k],T,Vdd,source_circuit)
+            file = open("../Characterization/"+str(i)+str(j)+str(k)+".cir", "w")
+            print("Netlist Build For " + str(i)+str(j)+str(k)+".cir" + " Complete ")
+            file.write(cir)
+            file.close()
+
+print("Netlists Built Successfully for all different combinations")
+
+print("Running Simulations")
+
+NgSpice_Run(input_nodes,input_slew,output_capacitance)
             
-    input_nodes = list()
-    i=1
-    temp = str(data[0][0]).split(",")
-    while(i<len(temp)):
-        if temp[i].isalnum() == True :
-            input_nodes.append(temp[i])
-        i=i+1
+Timing = [["cell_rise_time (ns)","cell_fall_time (ns)","output_rise_time (ns)","output_fall_time (ns)"]]
 
-    output_nodes = list()
-    i=1
-    temp = str(data[1][0]).split(",")
-    while(i<len(temp)):
-        if temp[i].isalnum() == True :
-            output_nodes.append(temp[i])
-        i=i+1
-
-    Vdd = float(data[2][0].split(",")[1])
-    T = float(data[3][0].split(",")[1])
-
-    input_slew = list()
-    i=1
-    temp = str(data[4][0]).split(",")
-    while(i<len(temp)):
-        if len(temp) > 0 :
-            m = str(float(temp[i][:-1])*100/60)+'n'
-            print(m)
-            input_slew.append(m)
-        i=i+1
-
-    output_capacitance = list()
-    i=1
-    temp = str(data[5][0]).split(",")
-    while(i<len(temp)):
-        if len(temp[i]) > 0 :
-            output_capacitance.append(temp[i])
-        i=i+1
-
-    
-    ##-----Processing Netlist-----##
-        
-    f = open("../Characterization/Netlists/"+ choice_file, "r")
-    circuit = f.read()
-    circuit = circuit.split("\n")
-    circuit.remove(".end")
-    circuit.append("\nV1 Vdd 0 " + str(Vdd))
-    source_circuit = circuit
-    temp_circuit = list()
-    cir = ''
-    print("Source Circuit Read")
-    print("Building Netlists")
-
-    ##----- Create Netlists for all different combinations-----##
-    for i in range(len(input_nodes)):
-        for j in range(len(input_slew)):
-            for k in range(len(output_capacitance)):
-                cir = Netlist_Build(output_nodes,input_nodes,input_nodes[i],input_slew[j],output_capacitance[k],T,Vdd,source_circuit)
-                file = open("../Characterization/"+str(i)+str(j)+str(k)+".cir", "w")
-                print("Netlist Build For " + str(i)+str(j)+str(k)+".cir" + " Complete ")
-                file.write(cir)
-                file.close()
-
-    print("Netlists Built Successfully for all different combinations")
-
-    print("Running Simulations")
-
-    NgSpice_Run(input_nodes,input_slew,output_capacitance)
-                
-    Timing = [["cell_rise_time (ns)","cell_fall_time (ns)","output_rise_time (ns)","output_fall_time (ns)"]]
-
-    print(" Characterization Algorithm beginning ... ")
-    print(" Analyzing Data files ")
-    m=1
-    file = open("../Characterization/" + input_read_file+ "Characterisation Results" + ".txt", "w")
-    string = ''
-    rise_transition = list()
-    cell_rise = list()
-    fall_transition = list()
-    cell_fall = list()
+print(" Characterization Algorithm beginning ... ")
+print(" Analyzing Data files ")
+m=1
+file = open("../Characterization/" + input_read_file+ "Characterisation Results" + ".txt", "w")
+string = ''
+rise_transition = list()
+cell_rise = list()
+fall_transition = list()
+cell_fall = list()
+input_slew_original = list()
+for i in range(len(input_slew)):
+    input_slew_original.append(str(float(input_slew[i][:-1])*60/100))
 ##    return [cell_rise_time*(10**9),cell_fall_time*(10**9),output_rise_time*(10**9),output_fall_time*(10**9)]
-    for i in range(len(input_nodes)):
-        for j in range(len(input_slew)):
-            for k in range(len(output_capacitance)):
-                Timing.append(Characterization_run(input_nodes,input_slew[j],output_capacitance[k],input_nodes[i]))
-                cell_rise.append(Timing[m][0])
-                cell_fall.append(Timing[m][1])
-                rise_transition.append(Timing[m][2])
-                fall_transition.append(Timing[m][3])
-                print(" Case : " + str(m) + "Input Node : " + input_nodes[i] + " Input Slew : " + input_slew[j] + " Output Capacitance : " + output_capacitance[k] + "Complete")
-                print(Timing[0])
-                print(Timing[m])
-                print("\n")
-                file.write(str(m) + ". Input Node : " + input_nodes[i] + " Input Slew : " + input_slew[j] + " Output Capacitance : " + output_capacitance[k])
-                file.write("\n")
-                file.write(str(Timing[0]))
-                file.write("\n")
-                file.write(str(Timing[m]))
-                file.write("\n")
-                file.write("\n")
-                m = m+1
-    
-    x = 0
-    file.close()
-    
-        
-        
-                
-    
-    Delete_Files(input_nodes,input_slew,output_capacitance)
+for i in range(len(input_nodes)):
+    for j in range(len(input_slew)):
+        for k in range(len(output_capacitance)):
+            Timing.append(Characterization_run(input_nodes,input_slew[j],output_capacitance[k],input_nodes[i],slew_lower_threshold,slew_upper_threshold,Vdd))
+            cell_rise.append(Timing[m][0])
+            cell_fall.append(Timing[m][1])
+            rise_transition.append(Timing[m][2])
+            fall_transition.append(Timing[m][3])
+            print(" Case : " + str(m) + "Input Node : " + input_nodes[i] + " Input Slew : " + input_slew_original[j] + " Output Capacitance : " + output_capacitance[k] + "Complete")
+            print(Timing[0])
+            print(Timing[m])
+            print("\n")
+            file.write(str(m) + ". Input Node : " + input_nodes[i] + " Input Slew : " + input_slew_original[j] + " Output Capacitance : " + output_capacitance[k])
+            file.write("\n")
+            file.write(str(Timing[0]))
+            file.write("\n")
+            file.write(str(Timing[m]))
+            file.write("\n")
+            file.write("\n")
+            m = m+1
+x = 0
+file.close()
+workbook = xlsxwriter.Workbook("../Characterization/"+input_read_file + 'Characterisation Results.xlsx')
 
-if __name__ == "__main__":
-    main()
-    
+##    name,workbook,input_nodes,output_capacitance,input_slew
+Excel_writer(" rise_transition ",workbook,input_nodes,output_capacitance,input_slew_original,rise_transition)
+Excel_writer(" cell_rise  ",workbook,input_nodes,output_capacitance,input_slew_original,cell_rise)
+Excel_writer(" fall_transition  ",workbook,input_nodes,output_capacitance,input_slew_original,fall_transition)
+Excel_writer(" cell_fall  ",workbook,input_nodes,output_capacitance,input_slew_original,cell_fall)
+workbook.close()
+Delete_Files(input_nodes,input_slew,output_capacitance,)
+
+##if __name__ == "__main__":
+##    main()
+##    
--- Python Files/xlsxWriter_character.py
+++ Python Files/xlsxWriter_character.py
@@ -1,74 +1,35 @@
 import xlsxwriter
-from random import random
-t_rise = list()
-for i in range(25):
-    t_rise.append(random())
-workbook = xlsxwriter.Workbook('Characterisation Results.xlsx')
-worksheet = workbook.add_worksheet(name = "Tfall")
-bold = workbook.add_format({'bold': 1})
-input_nodes = ['A','B']
-input_slew = [1.2,2.3,3.4,4.5,5.6]
-output_capacitance = [0.06,0.18,0.42,0.99,0.19]
-
-column = 'B'
-row = 1
-## Write Row Header
-for i in range(len(input_slew)):
-    index = column + str(row)
-    worksheet.write(index, str(input_slew[i]) + " ns",bold)
-    column = chr(ord(column)+1)
-
-column = 'A'
-row = 2 
-## Write Column Header
-for j in range(len(output_capacitance)):
-    index = column + str(row)
-    worksheet.write(index, str(output_capacitance[j]) + " pF",bold)
-    row = row+1
-
-
-column = 'B'
-row = 2
-m=0
-for i in range(len(input_slew)):
-    for j in range(len(output_capacitance)):
-        index = chr(ord(column)+i) + str(row+j)
-        worksheet.write(index, str(t_rise[m]) + " ns")
-        m=m+1
-
-# delete
-worksheet = workbook.add_worksheet(name = "asdvaskdjv")
-bold = workbook.add_format({'bold': 1})
-input_nodes = ['A','B']
-input_slew = [1.2,2.3,3.4,4.5,5.6]
-output_capacitance = [0.06,0.18,0.42,0.99,0.19]
-
-column = 'B'
-row = 1
-## Write Row Header
-for i in range(len(input_slew)):
-    index = column + str(row)
-    worksheet.write(index, str(input_slew[i]) + " ns",bold)
-    column = chr(ord(column)+1)
-
-column = 'A'
-row = 2 
-## Write Column Header
-for j in range(len(output_capacitance)):
-    index = column + str(row)
-    worksheet.write(index, str(output_capacitance[j]) + " pF",bold)
-    row = row+1
-
-
-column = 'B'
-row = 2
-m=0
-for i in range(len(input_slew)):
-    for j in range(len(output_capacitance)):
-        index = chr(ord(column)+i) + str(row+j)
-        worksheet.write(index, str(t_rise[m]) + " ns")
-        m=m+1
-
-workbook.close()
-        
     
+workbook = xlsxwriter.Workbook('Characterisation Results.xlsx')
+def Excel_writer(name,workbook,input_nodes,output_capacitance,input_slew):
+    bold = workbook.add_format({'bold': 1})
+    column = 'B'
+    row = 1
+    for k in range(len(input_nodes)):
+        worksheet = workbook.add_worksheet(name +" Input Node : " + input_nodes[i]) 
+        ## Write Row Header
+        for i in range(len(input_slew)):
+            index = column + str(row)
+            worksheet.write(index, str(input_slew[i]) + " ns",bold)
+            column = chr(ord(column)+1)
+
+        column = 'A'
+        row = 2
+        
+        ## Write Column Header
+        for j in range(len(output_capacitance)):
+            index = column + str(row)
+            worksheet.write(index, str(output_capacitance[j]) + " pF",bold)
+            row = row+1
+
+        column = 'B'
+        row = 2
+        m=0
+
+        table_size = len(input_slew)*len(output_capacitance)
+        for i in range(len(input_slew)):
+            for j in range(len(output_capacitance)):
+                index = chr(ord(column)+i) + str(row+j)
+                worksheet.write(index, str(t_rise[m+i*table_size]) + " ns")
+                m=m+1
+
